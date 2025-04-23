80f478d15f9ad15476b9b31abf97bea7ef064193
Hugo Norte <hugonorte@gmail.com>
2025-04-22 21:45:32 -0300

    first commit

---
+++ app/main.py
@@ -0,0 +1,19 @@
+from fastapi import FastAPI, Depends
+from sqlalchemy.orm import Session
+from . import user_crud, models, schemas
+from .database import SessionLocal, engine
+
+models.Base.metadata.create_all(bind=engine)
+
+app = FastAPI()
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+@app.post("/users/", response_model=schemas.UserResponse)
+def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
+    return user_crud.create_user(db=db, user=user)
